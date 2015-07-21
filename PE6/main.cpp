1b3397b17ad28deb1d237da0c779c931ec324426
jlegendary <j.legendary2@gmail.com>
2015-07-20 21:36:13 -0400

    Project Euler Problem 6

---
+++ PE6/main.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  PE6
+//
+//  Created by Lejendary on 6/28/15.
+//  Copyright (c) 2015 Lejendary. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int a, c;
+    int sumOS = 0;
+    int sumTot = 0;
+    int squSum = 0;
+    int squTot = 0;
+    int diff;
+    
+    // Sum of the squares
+    for (a=0; a<100; a++)
+    {
+        sumOS= pow(a+1,2);                            // sumOS = (a+1)^2
+        sumTot+=sumOS;                                // sumTot = sumTot + sumOS
+    }
+    
+    // Square of the sum
+    for (c=1; c<101; c++)
+    {
+        squSum+=c;                                    // squSum = squsum+c
+    }
+    squTot = pow(squSum, 2);                          // squTot = (squSum)^2
+    
+    //Difference
+    diff = squTot - sumTot;
+    
+    std::cout << "Sum of the square is: " << sumTot << std::endl;
+    std::cout << "Square of the sum is: " << squTot << std::endl;
+    std::cout << "Difference is: " << diff << std::endl;
+    
+    return 0;
+}
