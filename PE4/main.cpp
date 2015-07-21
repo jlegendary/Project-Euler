b42b814664306042d61a9fb85c17124372548d99
jlegendary <j.legendary2@gmail.com>
2015-07-20 21:32:59 -0400

    Project Euler Problem 4

---
+++ PE4/main.cpp
@@ -0,0 +1,66 @@
+//
+//  main.cpp
+//  PE4
+//
+//  Created by Lejendary on 6/25/15.
+//  Copyright (c) 2015 Lejendary. All rights reserved.
+//
+
+#include<iostream>
+
+int palindrome(int x)
+{
+    int num;
+    int reverse = 0;
+    int temp;
+    
+    num = x;
+    temp = num;
+    
+    // Is it palindrome
+    while (temp > 0)
+    {
+        reverse = reverse * 10;
+        reverse = reverse + temp%10;
+        temp/= 10;                              //temp = temp/10;
+    }
+    
+    // Return the number if palindrome
+    if (num == reverse)
+    {
+        return num;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+int main()
+{
+    int checkIfPalind;
+    int itIsPalind;
+    int highestPalind=0;
+    int fDig,sDig;
+    
+    // For 100 to 999
+    for(fDig=100;fDig<1000;fDig++)
+    {
+        for(sDig=100;sDig<1000;sDig++)
+        {
+            checkIfPalind = fDig*sDig;
+    // Check if palindrome
+            itIsPalind = palindrome(checkIfPalind);
+    // If the palindrome is higher than previous, it becomes the highest
+            if(itIsPalind>highestPalind)
+            {
+                highestPalind = itIsPalind;
+            }
+        }
+    }
+    //Output the highest palindrome
+    std::cout << "The largest palindrome from product of two 3-digits number is: " << highestPalind << std::endl;
+    
+    return 0;
+}
