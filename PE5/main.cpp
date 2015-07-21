1ae7d3a4a06b161d20453a71305d3bcddb440dc1
jlegendary <j.legendary2@gmail.com>
2015-07-20 21:33:56 -0400

    Project Euler Problem 5

---
+++ PE5/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  PE5
+//
+//  Created by Lejendary on 6/26/15.
+//  Copyright (c) 2015 Lejendary. All rights reserved.
+//
+
+#include <iostream>
+
+
+long gcd(long a, long b)
+{
+    while (b)
+    {
+        long prevB = b;
+        b = a % b;
+        a = prevB;
+    }
+    return a;
+}
+
+
+long lcm(long a, long b)
+{
+    return a * (b / gcd(a, b));
+}
+
+
+int main()
+{
+    long result = 20;
+    
+    for (long number = result - 1; number > 1; --number)
+    {
+        result = lcm(result, number);
+    }
+    
+    std::cout << result << std::endl;
+}
